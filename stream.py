d9c10482a9c12fd36de1debdacaa475d96b606a5
Ronald Suplina <tyger995@gmail.com>
2022-06-03 16:26:33 +0100

    StreamLit Additional Functions

--- stream.py
+++ stream.py
@@ -1,4 +1,15 @@
 import streamlit as st
+import pandas
+
+data = {
+    'Series_1': [1, 2, 3, 4, 5],
+    'Series_2': [10, 20, 30, 40, 50]
+}
+dataframe = pandas.DataFrame(data)
 st.title('My App')
 st.subheader("This is a Subheader")
 st.write("This is a test WebApp")
+st.write(dataframe)
+st.line_chart(dataframe)
+st.area_chart(dataframe)
+st.slider("Testing")
